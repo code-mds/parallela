c8ec358b847eda29b8932bffe7161dcf378b022c
code-mds <43860962+code-mds@users.noreply.github.com>
2019-03-09 14:48:00 +0100

    S3 E3

--- src/main/java/serie03/S3Esercizio3.java
+++ src/main/java/serie03/S3Esercizio3.java
@@ -3,10 +3,13 @@ package serie03;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class S3Worker implements Runnable {
     private final static int ITERATIONS = 10_000;
     private final static int MAX_SUM = 500;
+    private final static Lock lock = new ReentrantLock();
 
     private final int id;
     private final int[] numbers;
@@ -18,12 +21,21 @@ class S3Worker implements Runnable {
 
     @Override
     public void run() {
+        System.out.println("Worker" + id + " started");
+
         for (int i=0; i<ITERATIONS; i++) {
             int idx = ThreadLocalRandom.current().nextInt(0, numbers.length);
             int value = ThreadLocalRandom.current().nextInt(10, 51);
-            numbers[idx] += value;
-            if(numbers[idx] > MAX_SUM)
-                numbers[idx] = 0;
+
+            lock.lock();
+            try {
+                numbers[idx] += value;
+                if (numbers[idx] > MAX_SUM) {
+                    numbers[idx] = 0;
+                }
+            } finally {
+                lock.unlock();
+            }
 
             int delay = ThreadLocalRandom.current().nextInt(2, 6);
             try {
@@ -31,8 +43,8 @@ class S3Worker implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
+        System.out.println("Worker" + id + " completed");
     }
 }
 public class S3Esercizio3 {
@@ -41,7 +53,6 @@ public class S3Esercizio3 {
 
     public static void main(final String[] args) {
         int[] numbers = new int[ARRAY_LEN];
-
         final List<S3Worker> allWorkers = new ArrayList<>();
         final List<Thread> allThreads = new ArrayList<>();
 
@@ -60,5 +71,9 @@ public class S3Esercizio3 {
                 e.printStackTrace();
             }
         }
+
+        System.out.print("Final results: ");
+        for (int val : numbers)
+            System.out.print(val + " ");
     }
 }
