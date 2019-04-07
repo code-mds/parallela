44cf84033de406d0498a9646e74f474493767fd1
mds <mds750@gmail.com>
2019-04-07 22:40:51 +0200

    S7 E3

--- src/main/java/serie07/S7Esercizio3.java
+++ src/main/java/serie07/S7Esercizio3.java
@@ -1,9 +1,10 @@
 package serie07;
 
-import java.time.DateTimeException;
+import java.text.DateFormat;
 import java.util.*;
 
-class Lettera {
+final class Lettera {
+    private final DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
     private final Date data = new Date();
     private final String mittente;
     private final String destinatario;
@@ -15,91 +16,109 @@ class Lettera {
 
     @Override
     public String toString() {
-        return String.format("Mittente: %s, Destinatario: %s, Data: %s", mittente, destinatario, data);
+        return String.format("%s -> %s [%s]", mittente, destinatario, formatter.format(data));
     }
 }
 
 class Amico implements Runnable {
-    private final String nome;
+    private final static int TOTALE_LETTERE = 150;
+    private final String nomePersonale;
     private final String nomeAmico;
-    private int lettereDisponibili = 150;
+    private int lettereDisponibili = TOTALE_LETTERE;
+    private int lettereInviate = 0;
 
-    public Amico(String nome, String nomeAmico) {
-        this.nome = nome;
+    Amico(String nomePersonale, String nomeAmico) {
+        this.nomePersonale = nomePersonale;
         this.nomeAmico = nomeAmico;
     }
 
     @Override
     public void run() {
         Random random = new Random();
+        int lettereIniziali = 2 + random.nextInt(4);
+        for (int i=0; i<lettereIniziali; i++)
+            invia();
 
-        int lettere = 2 + random.nextInt(4);
-        inviaLettere(lettere);
-
-        while(lettereDisponibili > 0) {
-
-            try {
-                final int delay = 20 + random.nextInt(31);
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            rispondi();
+        System.out.println("START ");
+        while (lettereDisponibili > 0 || S7Esercizio3.riceviLettera(nomePersonale) != null) {
+            //delay();
+            if (lettereDisponibili > 0)
+                invia();
         }
     }
 
-    private void inviaLettere(int lettere) {
-        for (int i=0; i<lettere && lettereDisponibili > 0; i++) {
-            S7Esercizio3.inviaLettera(nome, nomeAmico);
-            lettereDisponibili--;
+    private void delay() {
+        Random random = new Random();
+        final int delay = 20 + random.nextInt(31);
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    private void rispondi() {
-        List<Lettera> lettere = S7Esercizio3.riceviLettere(nome);
-        inviaLettere(lettere.size());
+    private void invia() {
+        S7Esercizio3.inviaLettera(nomePersonale, nomeAmico);
+        lettereInviate++;
+        lettereDisponibili--;
+        System.out.printf("%s -> %s [%d/%d] %d\n", nomePersonale, nomeAmico, lettereInviate, TOTALE_LETTERE, lettereDisponibili);
     }
-
 }
 
+
 public class S7Esercizio3 {
-    private final static Map<String, List<Lettera>> caselleDiPosta = new HashMap<>();
     private final static String amico1 = "Amico1";
     private final static String amico2 = "Amico2";
+    final static List<Lettera> casella1 = new ArrayList<>();
+    final static List<Lettera> casella2 = new ArrayList<>();
 
     static void inviaLettera(String mittente, String destinatario) {
-        Lettera lettara = new Lettera(mittente, destinatario);
-        System.out.println("INVIO: " + lettara);
-
-        S7Esercizio3.caselleDiPosta.get(destinatario).add(lettara);
+        Lettera lettera = new Lettera(mittente, destinatario);
+        if (destinatario.equals(amico1)) {
+            synchronized (casella1) {
+                casella1.add(lettera);
+            }
+        } else {
+            synchronized (casella2) {
+                casella2.add(lettera);
+            }
+        }
     }
 
-    static List<Lettera> riceviLettere(String destinatario) {
-        List<Lettera> lettere = new ArrayList<>(caselleDiPosta.get(destinatario));
+    static Lettera riceviLettera(String destinatario) {
+        if (destinatario.equals(amico1)) {
+            synchronized (casella1) {
+                return getLettera(destinatario, casella1);
+            }
+        } else {
+            synchronized (casella2) {
+                return getLettera(destinatario, casella2);
+            }
+        }
+    }
 
-        System.out.println(destinatario + " riceve " + lettere.size()+ " lettere");
-
-        caselleDiPosta.get(destinatario).clear();
-        return lettere;
+    private static Lettera getLettera(String destinatario, List<Lettera> casella) {
+        int size = casella.size();
+        //System.out.println(destinatario + " lettere:" + size);
+        if (size > 0) {
+            Lettera l = casella.remove(0);
+            System.out.println("get " + l);
+            return l;
+        }
+        System.out.println("RETURN NULL");
+        return null;
     }
 
 
     public static void main(String[] args) {
         List<Thread> threads = new ArrayList<>();
-
         threads.add(new Thread(new Amico(amico1, amico2)));
-        caselleDiPosta.put(amico1, new ArrayList<>());
-
         threads.add(new Thread(new Amico(amico2, amico1)));
-        caselleDiPosta.put(amico2, new ArrayList<>());
 
-
-        for(final Thread t : threads) {
+        for (final Thread t : threads)
             t.start();
-        }
 
-        for(final Thread t : threads) {
+        for (final Thread t : threads) {
             try {
                 t.join();
             } catch (InterruptedException e) {
@@ -107,7 +126,21 @@ public class S7Esercizio3 {
             }
         }
 
-        System.out.println("Simulazione finita");
+        System.out.println("Simulazione finita: " + new Date().toString());
 
+        printCasella1();
+        printCasella2();
+    }
+
+    static void printCasella2() {
+        System.out.println("casella2:");
+        for (Lettera l : casella2)
+            System.out.println(l);
+    }
+
+    static void printCasella1() {
+        System.out.println("casella1:");
+        for (Lettera l : casella1)
+            System.out.println(l);
     }
 }
