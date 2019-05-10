72d779d4471a880f903a1a2bbf4a3d789c578c2b
code-mds <43860962+code-mds@users.noreply.github.com>
2019-05-10 18:08:22 +0200

    S8 E1

--- src/main/java/serie08/S8Esercizio1.java
+++ src/main/java/serie08/S8Esercizio1.java
@@ -13,9 +13,33 @@ class Adder implements Runnable {
 
 	@Override
 	public void run() {
-		// Calcola somma delle righe
-		S8Esercizio1.rowSum[idx] = S8Esercizio1.sumRow(idx);
-		S8Esercizio1.colSum[idx] = S8Esercizio1.sumColumn(idx);
+		System.out.println(idx + ") SOMMO RIGHE");
+		int sum = S8Esercizio1.sumRow(idx);
+		synchronized (S8Esercizio1.rowSum) {
+			S8Esercizio1.rowSum[idx] = sum;
+			while (S8Esercizio1.rowsToSum > 0) {
+				try {
+					S8Esercizio1.rowSum.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			S8Esercizio1.rowSum.notifyAll();
+		}
+
+		System.out.println(idx + ") SOMMO COLONNE");
+		sum = S8Esercizio1.sumColumn(idx);
+		synchronized (S8Esercizio1.colSum) {
+			S8Esercizio1.colSum[idx] = sum;
+			while (S8Esercizio1.colsToSum > 0) {
+				try {
+					S8Esercizio1.colSum.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			S8Esercizio1.colSum.notifyAll();
+		}
 	}
 }
 
@@ -25,9 +49,10 @@ class Matrix {
 
 public class S8Esercizio1 {
 	final static int[][] matrix = new int[10][10];
-	static volatile int rowsToSum = matrix.length;
 	final static int[] rowSum = new int[matrix.length];
 	final static int[] colSum = new int[matrix[0].length];
+	static int rowsToSum = rowSum.length;
+	static int colsToSum = colSum.length;
 
 	public static void main(String[] args) {
 		// Inizializza matrice con valori random
@@ -46,46 +71,60 @@ public class S8Esercizio1 {
 		}
 		threads.forEach(Thread::start);
 
-
-//		for (int row = 0; row < matrix.length; row++) {
-//			synchronized (S8Esercizio1.rowSum) {
-//				try {
-//					S8Esercizio1.rowSum.wait();
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}
-
-
-//			rowSum[row] = sumRow(row);
+		synchronized (rowSum){
+			while (rowsToSum > 0) {
+				try {
+					rowSum.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 		// Stampa somma delle righe
 		System.out.println("Somme delle righe:");
 		printArray(rowSum);
+		stampaTotaleRighe();
 
-		// Calcola somma delle colonne
-//		for (int col = 0; col < matrix[0].length; col++)
-//			for (int row = 0; row < matrix.length; row++) {
-//				synchronized (S8Esercizio1.colSum) {
-//					try {
-//						S8Esercizio1.colSum.wait();
-//					} catch (InterruptedException e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			}
-
+		synchronized (colSum){
+			while (colsToSum > 0) {
+				try {
+					colSum.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 		// Stampa somma delle colonne
 		System.out.println("Somme delle colonne:");
 		printArray(colSum);
+		stampaTotaleColonne();
+
+	}
+
+	private static void stampaTotaleColonne() {
+		int totale = 0;
+		for (int col = 0; col < colSum.length; col++) {
+			totale += colSum[col];
+		}
+		System.out.println("Totale Colonne: " + totale);
+	}
+
+	private static void stampaTotaleRighe() {
+		int totale = 0;
+		for (int row = 0; row < rowSum.length; row++) {
+			totale += rowSum[row];
+		}
+		System.out.println("Totale Righe: " + totale);
 	}
 
 	public static int sumRow(final int row) {
 		int result = 0;
 		for (int col = 0; col < matrix[row].length; col++)
 			result += matrix[row][col];
+
+		rowsToSum--;
 		return result;
 	}
 
@@ -93,6 +132,10 @@ public class S8Esercizio1 {
 		int temp = 0;
 		for (int col = 0; col < matrix.length; col++)
 			temp += matrix[col][row];
+
+		colsToSum--;
+
+
 		return temp;
 	}
 
