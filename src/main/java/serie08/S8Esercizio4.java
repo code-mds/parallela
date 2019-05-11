22772edf85cac3d323ca0e6f6bf4359a7844c507
mds <mds750@gmail.com>
2019-05-11 20:12:16 +0200

    S8 E4

--- src/main/java/serie08/S8Esercizio4.java
+++ src/main/java/serie08/S8Esercizio4.java
@@ -12,12 +12,13 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class S8Esercizio4 {
     private final static int NR_FANTINI = 10;
+    private final static boolean CONDITION_IMPL = true;
+
 
     public static void main(String[] args) {
         List<Thread> threads = new ArrayList<>();
         for (int i=0; i< NR_FANTINI; i++)
-            //threads.add(new Thread(new FantinoCondition(i)));
-            threads.add(new Thread(new FantinoSynchronizer(i)));
+            threads.add(new Thread(Fantino.build(i)));
 
         threads.forEach(Thread::start);
         for (Thread t : threads) {
@@ -29,14 +30,9 @@ public class S8Esercizio4 {
         }
     }
 
-
-    private static class FantinoCondition implements Runnable {
-        private static final Lock lock = new ReentrantLock();
-        private static final Condition condition = lock.newCondition();
-        private static int inLinea = 0;
-
+    private abstract static class Fantino implements Runnable {
         private final int idx;
-        FantinoCondition(int idx) {
+        Fantino(int idx) {
             this.idx = idx;
         }
 
@@ -49,7 +45,40 @@ public class S8Esercizio4 {
             System.out.printf("%s: ha atteso %d ms\n", this, elapsed);
         }
 
-        private void waitAll() {
+        protected abstract void waitAll();
+
+        private void arrivoLinea() {
+            int delay = ThreadLocalRandom.current().nextInt(1000, 1051);
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(this + ": arrivo alla linea di partenza");
+        }
+
+        @Override
+        public String toString() {
+            return "Fantino" + idx;
+        }
+
+        static Fantino build(int idx) {
+            return CONDITION_IMPL ?
+                    new FantinoCondition(idx):
+                    new FantinoSynchronizer(idx);
+        }
+    }
+
+    private static final class FantinoCondition extends Fantino {
+        private static final Lock lock = new ReentrantLock();
+        private static final Condition condition = lock.newCondition();
+        private static int inLinea = 0;
+
+        FantinoCondition(int idx) {
+            super(idx);
+        }
+
+        protected void waitAll() {
             lock.lock();
             try {
                 inLinea++;
@@ -63,60 +92,18 @@ public class S8Esercizio4 {
                 lock.unlock();
             }
         }
-
-        private void arrivoLinea() {
-            int delay = ThreadLocalRandom.current().nextInt(1000, 1051);
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(this + ": arrivo alla linea di partenza");
-        }
-
-        @Override
-        public String toString() {
-            return "Fantino" + idx;
-        }
     }
 
-    private static class FantinoSynchronizer implements Runnable {
+    private static class FantinoSynchronizer extends Fantino {
         private static final Phaser phaser = new Phaser();
 
-        private final int idx;
         FantinoSynchronizer(int idx) {
-            this.idx = idx;
+            super(idx);
             phaser.register();
         }
 
-        @Override
-        public void run() {
-            arrivoLinea();
-            Instant startTime = Instant.now();
-            waitAll();
-            long elapsed = Duration.between(startTime, Instant.now()).toMillis();
-            System.out.printf("%s: ha atteso %d ms\n", this, elapsed);
-
-            phaser.arriveAndDeregister();
-        }
-
-        private void waitAll() {
+        protected void waitAll() {
             phaser.arriveAndAwaitAdvance();
-        }
-
-        private void arrivoLinea() {
-            int delay = ThreadLocalRandom.current().nextInt(1000, 1051);
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(this + ": arrivo alla linea di partenza");
-        }
-
-        @Override
-        public String toString() {
-            return "Fantino" + idx;
         }
     }
 
