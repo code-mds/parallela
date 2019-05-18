0f7a08a6e9c479e88dae933bbb90cbfc71c587be
mds <mds750@gmail.com>
2019-05-18 16:53:38 +0200

    S9 E3

--- src/main/java/serie09/S9Factory.java
+++ src/main/java/serie09/S9Factory.java
@@ -1,8 +1,10 @@
 package serie09;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 class Depot {
 	final private int id;
@@ -15,6 +17,16 @@ class Depot {
 	}
 
 	public boolean isEmpty() {
+
+		// simulate long running process
+//		try {
+//			int val = ThreadLocalRandom.current().nextInt(1, 30);
+//			if(val > 15)
+//				Thread.sleep(val);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+
 		return elements.isEmpty();
 	}
 
@@ -49,7 +61,10 @@ class AssemblingWorker implements Runnable {
 		int failureCounter = 0;
 		while (true) {
 
-			// Choose randomly 3 different suppliers
+			// 5 worker running in parallel
+			// Choose randomly 3 different suppliers (from 10 available)
+			// add the selected supplier in a list
+
 			final List<Depot> depots = new ArrayList<>();
 			while (depots.size() != 3) {
 				final Depot randomDepot = S9Factory.suppliers[random.nextInt(S9Factory.suppliers.length)];
@@ -57,10 +72,31 @@ class AssemblingWorker implements Runnable {
 					depots.add(randomDepot);
 			}
 
+			// the current implementation is nesting synchronized blocks on different object
+			// but we can run on deadlock since the order of lock acquisition is random
+
+			// try to fix sorting the list by id
+			depots.sort(Comparator.comparingInt(Depot::getId));
+
+			// Th1: 1,3,5   1		3(w2)  	        5
+			// Th2: 3,4,5    3		 4(w3)	       5
+			// Th3: 4,5,6     4		  5(w4) 	  6
+			// Th4: 5,6,8      5	   6(w5)	 8
+			// Th5: 6,7,9     	6	    7		9
+
+			// Th1: 3,4,5   3			4			5
+			// Th2: 2,3,4    2			 3(w1)		 4(w4)
+			// Th3: 1,2,3     1			  2(w2)		  3(w2)
+			// Th4: 3,4,5      3(w1)	   4(w1)		5(w1)
+			// Th5: 1,2,3     	1(w3)	    2(w2)		 3(w4)
+
+
 			final Depot supplier1 = depots.get(0);
 			final Depot supplier2 = depots.get(1);
 			final Depot supplier3 = depots.get(2);
 
+			// in order to assemble a product we need to be sure that the 3 supplier are not empty
+			// if any of them is empty we increase the failureCounter and retry the loop
 			log("assembling from : " + supplier1 + ", " + supplier2 + ", " + supplier3);
 			synchronized (supplier1) {
 				if (supplier1.isEmpty()) {
